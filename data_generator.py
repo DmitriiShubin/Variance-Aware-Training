ea8c95083f30369d795d4a62b907aeed8cfa34ab
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-10-25 13:41:19 -0400

    removed augs

--- data_generator.py
+++ data_generator.py
@@ -115,7 +115,7 @@ class Preprocessing:
     def __init__(self, aug):
 
         self.aug = aug
-        self.augmentations = Augmentations(0.5)
+        self.augmentations = Augmentations(0.0)
 
     def run(self, X, y, label_process=True):
 
