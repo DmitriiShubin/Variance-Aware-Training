f91770751d4f68dce64a2efec51a1f52f9cbcae3
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-10-26 13:15:42 -0400

    lr 1e-5, drop 0.1, N-filt 64

--- models/unet/model_config.py
+++ models/unet/model_config.py
@@ -3,7 +3,7 @@ import os
 hparams = {}
 # training params
 hparams['n_epochs'] = 2
-hparams['lr'] = 1e-4
+hparams['lr'] = 1e-5
 hparams['batch_size'] = 2
 hparams['verbose_train'] = True
 
@@ -26,10 +26,10 @@ for path in [hparams['model_path'], hparams['checkpoint_path']]:
 # dictionary of hyperparameters
 structure_hparams = dict()
 # global dropout rate
-structure_hparams['dropout'] = 0.0
+structure_hparams['dropout'] = 0.1
 structure_hparams['alpha'] = 0.1
 # number of filers for the models
 structure_hparams['kernel_size'] = 3  # must be odd
-structure_hparams['n_filters_input'] = 32
+structure_hparams['n_filters_input'] = 64
 
 hparams['model'] = structure_hparams
