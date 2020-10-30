ce5deaea0e7a5cb63d791576a69797a6c3d64818
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-10-29 23:08:54 -0400

    alpha 1 for one of sh scripts

--- brats_experiment/train_0_alpha_2.sh
+++ brats_experiment/train_0_alpha_2.sh
@@ -1,1 +1,1 @@
-python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 0.001 --gpu 6,7 --alpha 0.2
+python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 0.001 --gpu 6,7 --alpha 1
