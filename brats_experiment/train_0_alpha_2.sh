4e39ff3aa6235c05b8de635c64c3ca16f092175a
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-10-30 15:35:23 -0400

    0.01 with novel config

--- brats_experiment/train_0_alpha_2.sh
+++ brats_experiment/train_0_alpha_2.sh
@@ -1,1 +1,1 @@
-python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1 --alpha 1 --model adv_unet
+python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1 --alpha 0.01 --model adv_unet
