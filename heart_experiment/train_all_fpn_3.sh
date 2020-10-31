98791e163fb4d79e0c6aa6ea7a5dbc7236af441b
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-10-31 17:52:33 -0400

    fixed minor bugs

--- heart_experiment/train_all_fpn_3.sh
+++ heart_experiment/train_all_fpn_3.sh
@@ -5,7 +5,7 @@
 #
 #done
 
-python main.py --start_fold 4 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 4,5,6 --model adv_fpn &
+python main.py --start_fold 4 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --model adv_fpn &
 python main.py --start_fold 3 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 4,5,6 --model adv_fpn &
 
 
