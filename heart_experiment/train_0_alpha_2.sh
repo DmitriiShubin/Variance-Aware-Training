88d695601b8752b737c9bfadfc6d8a53dd46a148
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-10-31 16:51:39 -0400

    added gpu for 2 exps for heart dataset

--- heart_experiment/train_0_alpha_2.sh
+++ heart_experiment/train_0_alpha_2.sh
@@ -1,1 +1,1 @@
-python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 2,3 --alpha 0.1 --model adv_unet
+python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 1,2,3 --alpha 0.1 --model adv_unet
