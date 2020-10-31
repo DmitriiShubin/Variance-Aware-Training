58e5cfe90ae4de8c64742660480650b158ff56ea
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-10-31 17:51:31 -0400

    added palallel scripts for all models

---
+++ heart_experiment/train_all_fpn_1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#Train models for unet
+#for FOLD in 0 1 2 3 4
+#do
+#
+#done
+
+python main.py --start_fold 2 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 1,0 --model fpn &
+python main.py --start_fold 3 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 2,3 --model fpn &
+python main.py --start_fold 1 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 4,5,6 --model adv_fpn &
+
+
+
