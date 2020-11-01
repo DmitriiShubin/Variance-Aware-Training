ab41472b4a36ed8d43d278132e70f29a3a2c5bde
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-01 02:55:41 -0500

    heart: fpn 0.1 alpha

--- heart_experiment/train_0_alpha_2.sh
+++ heart_experiment/train_0_alpha_2.sh
@@ -1,1 +1,1 @@
-python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 1,2,3 --alpha 0.01 --model adv_fpn
+python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 1,2,3 --alpha 0.1 --model adv_fpn
