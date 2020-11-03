734cd8ad98ffd4bd230bff6ec7afde33d330274c
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-03 14:00:46 -0500

    last model

--- heart_experiment/train_all_unet_2.sh
+++ heart_experiment/train_all_unet_2.sh
@@ -6,7 +6,7 @@
 #done
 
 python main.py --start_fold 4 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 1,0 --model unet &
-python main.py --start_fold 2 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --alpha 0.005 --model adv_unet &
+python main.py --start_fold 2 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 4,5,6 --alpha 0.005 --model adv_unet &
 
 
 
