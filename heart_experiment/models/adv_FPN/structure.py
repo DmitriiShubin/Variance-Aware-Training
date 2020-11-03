b96983ef57a88fd4860acc34c0818d05c88931bf
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-03 15:16:55 -0500

    fixed all bugs

--- heart_experiment/models/adv_FPN/structure.py
+++ heart_experiment/models/adv_FPN/structure.py
@@ -43,8 +43,8 @@ class FPN(smp_FPN):
 
         # adversarial deep net layers
         self.adv_conv1 = nn.Conv2d(self.hparams['n_filters_input'] * 2, 1, kernel_size=1, padding=0)
-        self.adv_fc1 = nn.Linear(self.hparams['n_filters_input'], 1)
-        self.adv_fc2 = nn.Linear(self.hparams['n_filters_input'], 1)
+        self.adv_fc1 = nn.Linear(320, 1)
+        #self.adv_fc2 = nn.Linear(self.hparams['n_filters_input'], 1)
 
     # def forward(self, x):
     #     x1, x2, x3, x4, x5 = self.encoder(x)
