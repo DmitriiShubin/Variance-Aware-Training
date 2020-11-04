d10cf965bd523702888f0a946fd3b1c6177201d1
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-03 19:48:56 -0500

    added s slpeen experiment

---
+++ spleen_experiment/train_3.sh
@@ -0,0 +1,1 @@
+python main.py --start_fold 3 --n_epochs 150 --batch_size 64 --lr 0.001 --gpu 6,7
---
+++ spleen_experiment/train_all_fpn_3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Train models for unet
+#for FOLD in 0 1 2 3 4
+#do
+#
+#done
+
+python main.py --start_fold 4 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 3,4,5 --alpha 0.05 --model adv_fpn &
+python main.py --start_fold 3 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 3,4,5 --alpha 0.05 --model adv_fpn &
+
+
+
