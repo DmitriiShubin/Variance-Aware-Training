bafc10a21df256e71547245cb2282126932bd47d
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-04 14:50:34 -0500

    prostate: adv_unet num workers 40

--- prostate_experiment/models/adv_unet/model.py
+++ prostate_experiment/models/adv_unet/model.py
@@ -60,7 +60,7 @@ class Model:
                 print('Only one GPU is available')
 
         self.metric = Metric()
-        self.num_workers = 35
+        self.num_workers = 40
 
         ########################## compile the model ###############################
 
