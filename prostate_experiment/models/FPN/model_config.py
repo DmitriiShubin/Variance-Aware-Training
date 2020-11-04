d597b860785a322cb921fc6b8af30b1e27a6bb31
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-04 13:22:16 -0500

    prostate: fpn 64 filt 0.1 drop

--- prostate_experiment/models/FPN/model_config.py
+++ prostate_experiment/models/FPN/model_config.py
@@ -26,10 +26,10 @@ for path in [hparams['model_path'], hparams['checkpoint_path']]:
 # dictionary of hyperparameters
 structure_hparams = dict()
 # global dropout rate
-structure_hparams['dropout'] = 0.2
+structure_hparams['dropout'] = 0.1
 structure_hparams['alpha'] = 0.1
 # number of filers for the models
 structure_hparams['kernel_size'] = 3  # must be odd
-structure_hparams['n_filters_input'] = 128
+structure_hparams['n_filters_input'] = 64
 
 hparams['model'] = structure_hparams
