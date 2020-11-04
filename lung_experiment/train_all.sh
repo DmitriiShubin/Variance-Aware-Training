6b5352d3cdf754034a628fe642d3baa4f8a137ed
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-04 03:10:14 -0500

    added gpu for lung exp for test

--- lung_experiment/train_all.sh
+++ lung_experiment/train_all.sh
@@ -2,7 +2,7 @@
 #Train models for unet
 for FOLD in 0 1 2 3 4
 do
-  python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2,3 --model unet
+  python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2,3  --model unet
   python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2,3 --alpha 0.001 --model adv_unet
   python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2,3 --model fpn
   python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2,3 --alpha 0.05 --model adv_fpn
