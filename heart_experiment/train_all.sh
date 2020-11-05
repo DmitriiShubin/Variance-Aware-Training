a9294a20cbae88ab3b6d5207a19608e8e85f1048
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-05 02:53:27 -0500

    heart: fixed sh scripts

--- heart_experiment/train_all.sh
+++ heart_experiment/train_all.sh
@@ -5,7 +5,7 @@ do
   python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --model unet
   python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --alpha 0.001 --model adv_unet
   python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --model fpn
-  python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --alpha 0.2 --model adv_fpn
+  python main.py --start_fold $FOLD --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2,3 --alpha 0.2 --model adv_fpn
 done
 
 
