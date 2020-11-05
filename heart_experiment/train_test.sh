5346cdb45c10913bea77defbef91f29dc949c6c2
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-05 02:30:57 -0500

    heart: prepared all

--- heart_experiment/train_test.sh
+++ heart_experiment/train_test.sh
@@ -1,7 +1,7 @@
 for FOLD in 0
 do
-  python main.py --start_fold $FOLD --n_epochs 2 --batch_size 32 --lr 1e-4 --gpu 1,0 --model unet
-  python main.py --start_fold $FOLD --n_epochs 2 --batch_size 32 --lr 1e-4 --gpu 4,5,6 --alpha 0.001 --model adv_unet
-  python main.py --start_fold $FOLD --n_epochs 2 --batch_size 32 --lr 1e-4 --gpu 2,3 --model fpn
-  python main.py --start_fold $FOLD --n_epochs 2 --batch_size 32 --lr 1e-4 --gpu 3,4,5 --alpha 0.05 --model adv_fpn
+  python main.py --start_fold $FOLD --n_epochs 2 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --model unet
+  python main.py --start_fold $FOLD --n_epochs 2 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --alpha 0.001 --model adv_unet
+  python main.py --start_fold $FOLD --n_epochs 2 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --model fpn
+  python main.py --start_fold $FOLD --n_epochs 2 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --alpha 0.2 --model adv_fpn
 done
