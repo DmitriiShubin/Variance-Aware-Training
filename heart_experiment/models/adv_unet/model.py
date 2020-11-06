60255b2056e27a61a28b0622a66a42555b694b88
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-05 23:57:10 -0500

    heart: num work 9

--- heart_experiment/models/adv_unet/model.py
+++ heart_experiment/models/adv_unet/model.py
@@ -61,7 +61,7 @@ class Model:
                 print('Only one GPU is available')
 
         self.metric = Metric()
-        self.num_workers = 32
+        self.num_workers = 9
 
         ########################## compile the model ###############################
 
