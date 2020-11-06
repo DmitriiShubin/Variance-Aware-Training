5ed7e675956ded0020a8a6b687b9f5bd40cf8a74
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-05 23:45:00 -0500

    heart: added seeds

--- heart_experiment/models/adv_unet/model.py
+++ heart_experiment/models/adv_unet/model.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingLR
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
+import random
 
 # custom modules
 from metrics import Metric
@@ -101,10 +102,15 @@ class Model:
 
         self.scaler = torch.cuda.amp.GradScaler()
 
-    def seed_everything(self, seed):
+    def seed_everything(self, seed, eps=10):
         np.random.seed(seed)
+        random.seed(seed)
         os.environ['PYTHONHASHSEED'] = str(seed)
         torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.determenistic = True
+        torch.backends.cudnn.benchmark = False
+        torch.set_printoptions(precision=eps)
 
     def fit(self, train, valid):
 
