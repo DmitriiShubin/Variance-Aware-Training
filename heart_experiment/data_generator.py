4e4f45684a5c4e6f86c822173605861fb60a274b
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-05 23:52:06 -0500

    heart: fixed bug in datagenerator

--- heart_experiment/data_generator.py
+++ heart_experiment/data_generator.py
@@ -61,7 +61,7 @@ class Dataset_train(Dataset):
         X, y = self.preprocessing.run(X=X, y=y)
 
         # second head
-        sampled_patient = np.round(np.random.uniform(size=1)[0])
+        sampled_patient = np.round(np.random.uniform(size=1)[0],1)
         if sampled_patient >= 0.5:
             # NOT the same patient
             images_subset = self.images_list.copy()
