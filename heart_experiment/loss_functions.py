63278120ef664a97cb4bce2e43a24103e66d2eee
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-08 14:56:54 -0500

    dice loss 1-

--- heart_experiment/loss_functions.py
+++ heart_experiment/loss_functions.py
@@ -14,7 +14,7 @@ class Dice_loss(nn.Module):
         # y_truef = torch.flatten(y_true)
         # y_predf = torch.flatten(y_pred)
         And = torch.sum(y_true * y_pred, dim=0)
-        return -1 * torch.mean(
+        return 1 - torch.mean(
             (2 * And + self.smoothing)
             / (torch.sum(y_true, dim=0) + torch.sum(y_pred, dim=0) + self.smoothing)
         )
