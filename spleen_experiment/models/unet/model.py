9b1715b099ed39072b93653a932b2f9cd24575ec
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-09 02:35:51 -0500

    spleen 0 workers

--- spleen_experiment/models/unet/model.py
+++ spleen_experiment/models/unet/model.py
@@ -59,7 +59,7 @@ class Model:
                 print('Only one GPU is available')
 
         self.metric = Metric()
-        self.num_workers = 32
+        self.num_workers = 0
 
         ########################## compile the model ###############################
 
