1df22616156daaee85c8e0801360cb5117b2c4d5
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-09 09:26:27 -0500

    sh training loop

--- heart_experiment/train_0_alpha_0.sh
+++ heart_experiment/train_0_alpha_0.sh
@@ -1,4 +1,4 @@
-python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1 --alpha 0.01 --model unet
+#python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1 --alpha 0.01 --model unet
 #
 python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --alpha 0.1 --model adv_unet
 python main.py --start_fold 0 --n_epochs 100 --batch_size 32 --lr 1e-4 --gpu 0,1,2 --alpha 0.05 --model adv_unet
