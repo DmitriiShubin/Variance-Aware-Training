b69794ecbc8f2a90aeaf1a6f3274c05eed103236
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-08 22:47:51 -0500

    jaccard loss reimplemented

--- heart_experiment/loss_functions.py
+++ heart_experiment/loss_functions.py
@@ -27,7 +27,15 @@ class Jaccard_loss(nn.Module):
         self.smoothing = 1
 
     def forward(self, y_pred, y_true):
-        Intersection = torch.sum(y_true * y_pred, dim=0)
-        Union = torch.sum(y_true + y_pred, dim=0) - Intersection
-        loss = -1 * torch.mean((Intersection+ self.smoothing)/(Union+ self.smoothing))
-        return loss
+        y_true = y_true[:,1]
+        y_pred = y_pred[:, 1]
+        # Intersection = torch.sum(y_true * y_pred, dim=0)
+        # Union = torch.sum(y_true + y_pred, dim=0) - Intersection
+        # loss = -1 * torch.mean((Intersection+ self.smoothing)/(Union+ self.smoothing))
+
+        tp = torch.sum(y_true * y_pred, dim=0)
+        fp = torch.sum((1 - y_true) * y_pred, dim=0)
+        fn = torch.sum(y_true * (1 - y_pred), dim=0)
+        loss = (tp) / (tp + 1 * (fp + fn + self.smoothing))
+
+        return -1*loss
