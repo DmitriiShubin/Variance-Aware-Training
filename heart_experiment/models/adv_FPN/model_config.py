bccc2f765b753f93a5fdff0900be7062935255ab
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-10 02:33:58 -0500

    fpn 32 filt

--- heart_experiment/models/adv_FPN/model_config.py
+++ heart_experiment/models/adv_FPN/model_config.py
@@ -30,6 +30,6 @@ structure_hparams['dropout'] = 0.0
 structure_hparams['alpha'] = 0.1
 # number of filers for the models
 structure_hparams['kernel_size'] = 3  # must be odd
-structure_hparams['n_filters_input'] = 64
+structure_hparams['n_filters_input'] = 32
 
 hparams['model'] = structure_hparams
