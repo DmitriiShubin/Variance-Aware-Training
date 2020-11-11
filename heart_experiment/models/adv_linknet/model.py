1e14b42dd6c3c45c6ec341e856ef428bcb3ee458
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-11 01:52:18 -0500

    added threshold for weights

--- heart_experiment/models/adv_linknet/model.py
+++ heart_experiment/models/adv_linknet/model.py
@@ -175,7 +175,11 @@ class Model:
                 threshold = torch.log(torch.tensor([1/(threshold*lam)]).to(self.device))
                 weights = torch.mean(weights)
 
-                train_loss = train_loss + self.alpha *(1-torch.log(adv_loss)) + torch.min(torch.log(1/(lam*weights)),threshold)
+                if torch.log(1/(lam*weights)) < threshold:
+                    train_loss = train_loss + self.alpha *(1-torch.log(adv_loss))
+                else:
+                    train_loss = train_loss + self.alpha *(1-torch.log(adv_loss)) + torch.log(1 / (lam * weights))
+
                 weights = weights.cpu().detach()
                 threshold = threshold.cpu().detach()
 
