2a3ddad321d18aedd1250cc936c64cbfb682350f
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-11-12 00:49:24 -0500

    prostate delay 25

--- heart_experiment/data_generator.py
+++ heart_experiment/data_generator.py
@@ -115,7 +115,7 @@ class Preprocessing:
             else:
                 X[:, :, i] = X[:, :, i] - np.mean(X[:, :, i])
 
-        y[np.where(y ==2)] = 1
+
         y = np.eye(2, dtype=np.float32)[y.astype(np.int8)]
         y = y.reshape(y.shape[0], y.shape[1], y.shape[-1])
 
