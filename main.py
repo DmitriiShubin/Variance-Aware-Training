c93adbd33f801df78598b13dfa5b82b34eaa636b
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2021-06-06 16:28:00 -0400

    removed extra files, cleaned up main.py

--- main.py
+++ main.py
@@ -3,15 +3,6 @@
 # baseline models
 from experiments.segmentation.baseline.run_experiment import run as run_baseline_segmentation
 
-# pre-training encoders
-from experiments.segmentation.patch_encoder.run_experiment import run as run_pre_training_patch_segmentation
-from experiments.segmentation.contrastive_loss_encoder.run_experiment import (
-    run as run_pre_training_contrastive_segmentation,
-)
-from experiments.segmentation.rotation_encoder.run_experiment import (
-    run as run_pre_training_rotation_segmentation,
-)
-
 # pre-trained models
 from experiments.segmentation.pre_trained_patch.run_experiment import (
     run as run_pre_trained_patch_segmentation,
@@ -25,10 +16,28 @@ from experiments.segmentation.pre_trained_rotation.run_experiment import (
 
 # adversarial models
 from experiments.segmentation.adversarial_network_train_val_early.run_experiment import (
-    run as run_adversarial_network_train_val_early,
+    run as run_adv_early_segmentation,
 )
 from experiments.segmentation.adversarial_network_train_val_late.run_experiment import (
-    run as run_adversarial_network_train_val_late,
+    run as run_adv_late_segmentation,
+)
+
+################################################
+
+# HIST experiments:
+
+# baseline models
+from experiments.classification.baseline.run_experiment import run as run_baseline_classification
+
+# pre-trained models
+from experiments.classification.pre_trained.run_experiment import run as run_pre_trained_classification
+
+# adversarial models
+from experiments.classification.adversarial_network_train_val_early.run_experiment import (
+    run as run_adv_early_classification,
+)
+from experiments.classification.adversarial_network_train_val_late.run_experiment import (
+    run as run_adv_late_classification,
 )
 
 ################################################
@@ -36,31 +45,17 @@ from experiments.segmentation.adversarial_network_train_val_late.run_experiment 
 # APTOS experiments:
 
 # baseline models
-from experiments.regression.baseline.run_experiment import run as run_efficientnet_baseline_regression
-
-# pre-training models
-
-
-# adversarial models
-from experiments.regression.adversarial_network_train_val_early.run_experiment import (
-    run as run_efficientnet_adv_early_regression,
-)
-from experiments.regression.adversarial_network_train_val_late.run_experiment import (
-    run as run_efficientnet_adv_late_regression,
-)
+from experiments.regression.baseline.run_experiment import run as run_baseline_regression
 
 # pre-trained models
 from experiments.regression.pre_trained.run_experiment import run as run_pre_trained_regression
 
-################################################
-
-# HIST experiments:
-
-# baseline models
-from experiments.classification.baseline.run_experiment import run as run_efficientnet_baseline_classification
-
-from experiments.classification.adversarial_network_train_val_early.run_experiment import (
-    run as run_efficientnet_adv_early_classification,
+# adversarial models
+from experiments.regression.adversarial_network_train_val_early.run_experiment import (
+    run as run_adv_early_regression,
+)
+from experiments.regression.adversarial_network_train_val_late.run_experiment import (
+    run as run_adv_late_regression,
 )
 
 
@@ -76,93 +71,185 @@ import click
 @click.option('--gpu', default='7', help='')
 def main(experiment, gpu):
 
-
-
     # ACDC
 
-    #baseline, without pre-train
-    run_baseline_segmentation(experiment='./experiments/baseline/config_ACDC_2.yml')
-    run_baseline_segmentation(experiment='./experiments/baseline/config_ACDC_4.yml')
-    run_baseline_segmentation(experiment='./experiments/baseline/config_ACDC_8.yml')
-    run_baseline_segmentation(experiment='./experiments/baseline/config_ACDC_UB.yml')
-
-    #pre-training
-    run_pre_training_contrastive_segmentation(experiment='./experiments/contrastive_loss_encoder/config_ACDC.yml',gpu='7')
-    run_pre_training_rotation_segmentation(experiment='./experiments/rotation_encoder/config_ACDC.yml',gpu='7')
-    run_pre_training_patch_segmentation(experiment='./experiments/patch_encoder/config_ACDC.yml',gpu='6')
+    # baseline, without pre-train
+    run_baseline_segmentation(experiment='./experiments/segmentation/baseline/config_ACDC_2.yml')
+    run_baseline_segmentation(experiment='./experiments/segmentation/baseline/config_ACDC_4.yml')
+    run_baseline_segmentation(experiment='./experiments/segmentation/baseline/config_ACDC_8.yml')
+    run_baseline_segmentation(experiment='./experiments/segmentation/baseline/config_ACDC_UB.yml')
 
     # pre-trained contrastive
-    run_pre_trained_contrastive_segmentation(experiment='./experiments/pre_trained_contrastive/config_ACDC_2.yml')
-    run_pre_trained_contrastive_segmentation(experiment='./experiments/pre_trained_contrastive/config_ACDC_4.yml')
-    run_pre_trained_contrastive_segmentation(experiment='./experiments/pre_trained_contrastive/config_ACDC_8.yml')
+    run_pre_trained_contrastive_segmentation(
+        experiment='./experiments/segmentation/pre_trained_contrastive/config_ACDC_2.yml'
+    )
+    run_pre_trained_contrastive_segmentation(
+        experiment='./experiments/segmentation/pre_trained_contrastive/config_ACDC_4.yml'
+    )
+    run_pre_trained_contrastive_segmentation(
+        experiment='./experiments/segmentation/pre_trained_contrastive/config_ACDC_8.yml'
+    )
 
     # pre-trained rotation
-    run_pre_trained_rotation_segmentation(experiment='./experiments/pre_trained_rotation/config_ACDC_2.yml')
-    run_pre_trained_rotation_segmentation(experiment='./experiments/pre_trained_rotation/config_ACDC_4.yml')
-    run_pre_trained_rotation_segmentation(experiment='./experiments/pre_trained_rotation/config_ACDC_8.yml')
+    run_pre_trained_rotation_segmentation(
+        experiment='./experiments/segmentation/pre_trained_rotation/config_ACDC_2.yml'
+    )
+    run_pre_trained_rotation_segmentation(
+        experiment='./experiments/segmentation/pre_trained_rotation/config_ACDC_4.yml'
+    )
+    run_pre_trained_rotation_segmentation(
+        experiment='./experiments/segmentation/pre_trained_rotation/config_ACDC_8.yml'
+    )
 
     # pre-trained patch
-    run_pre_trained_patch_segmentation(experiment='./experiments/pre_trained_patch/config_ACDC_2.yml')
-    run_pre_trained_patch_segmentation(experiment='./experiments/pre_trained_patch/config_ACDC_4.yml')
-    run_pre_trained_patch_segmentation(experiment='./experiments/pre_trained_patch/config_ACDC_8.yml')
+    run_pre_trained_patch_segmentation(
+        experiment='./experiments/segmentation/pre_trained_patch/config_ACDC_2.yml'
+    )
+    run_pre_trained_patch_segmentation(
+        experiment='./experiments/segmentation/pre_trained_patch/config_ACDC_4.yml'
+    )
+    run_pre_trained_patch_segmentation(
+        experiment='./experiments/segmentation/pre_trained_patch/config_ACDC_8.yml'
+    )
 
-    Single-stage self-supervised early flat
-    run_adversarial_network_train_val_early(
+    # variance-aware, ealy agg
+    run_adv_early_segmentation(
         experiment='./experiments/segmentation/adversarial_network_train_val_early/config_ACDC_2.yml'
     )
-    run_adversarial_network_train_val_early(experiment='./experiments/segmentation/adversarial_network_train_val_early/config_ACDC_4.yml')
-    run_adversarial_network_train_val_early(experiment='./experiments/segmentation/adversarial_network_train_val_early/config_ACDC_8.yml')
+    run_adv_early_segmentation(
+        experiment='./experiments/segmentation/adversarial_network_train_val_early/config_ACDC_4.yml'
+    )
+    run_adv_early_segmentation(
+        experiment='./experiments/segmentation/adversarial_network_train_val_early/config_ACDC_8.yml'
+    )
 
-    Single-stage self-supervised late flat
-    run_adversarial_network_train_val_late(experiment='./experiments/adversarial_network_train_val_late/config_ACDC_2.yml')
-    run_adversarial_network_train_val_late(experiment='./experiments/adversarial_network_train_val_late/config_ACDC_4.yml')
-    run_adversarial_network_train_val_late(experiment='./experiments/adversarial_network_train_val_late/config_ACDC_8.yml')
+    # variance-aware, late agg
+    run_adv_late_segmentation(
+        experiment='./experiments/segmentation/adversarial_network_train_val_late/config_ACDC_2.yml'
+    )
+    run_adv_late_segmentation(
+        experiment='./experiments/segmentation/adversarial_network_train_val_late/config_ACDC_4.yml'
+    )
+    run_adv_late_segmentation(
+        experiment='./experiments/segmentation/adversarial_network_train_val_late/config_ACDC_8.yml'
+    )
+
+    ###########################################################################
+
+    # PCam (classification) experiments
+
+    # baseline, without pre-train
+    run_baseline_classification(experiment='./experiments/classification/baseline/config_HIST_2.yml')
+    run_baseline_classification(experiment='./experiments/classification/baseline/config_HIST_4.yml')
+    run_baseline_classification(experiment='./experiments/classification/baseline/config_HIST_8.yml')
+    run_baseline_classification(experiment='./experiments/classification/baseline/config_HIST_UB.yml')
+
+    # pre-trained contrastive (SimCLR)
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_2_contrastive.yml'
+    )
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_4_contrastive.yml'
+    )
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_8_contrastive.yml'
+    )
+
+    # pre-trained rotation
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_2_rotation.yml'
+    )
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_4_rotation.yml'
+    )
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_8_rotation.yml'
+    )
+
+    # pre-trained patch (CP)
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_2_patch.yml'
+    )
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_4_patch.yml'
+    )
+    run_pre_trained_classification(
+        experiment='./experiments/classification/pre_trained/config_HIST_8_patch.yml'
+    )
+
+    # variance-aware, ealy agg
+    run_adv_early_classification(
+        experiment='./experiments/classification/adversarial_network_train_val_early/config_HIST_2.yml'
+    )
+    run_adv_early_classification(
+        experiment='./experiments/classification/adversarial_network_train_val_early/config_HIST_4.yml'
+    )
+    run_adv_early_classification(
+        experiment='./experiments/classification/adversarial_network_train_val_early/config_HIST_8.yml'
+    )
+
+    # variance-aware, late agg
+    run_adv_late_classification(
+        experiment='./experiments/classification/adversarial_network_train_val_late/config_HIST_2.yml'
+    )
+    run_adv_late_classification(
+        experiment='./experiments/classification/adversarial_network_train_val_late/config_HIST_4.yml'
+    )
+    run_adv_late_classification(
+        experiment='./experiments/classification/adversarial_network_train_val_late/config_HIST_8.yml'
+    )
 
     ###########################################################################
     # APTOS
 
-    # baseline models
-    #run_efficientnet_baseline_regression(experiment='./experiments/regression/baseline/config_aptos_2.yml', gpu='0,1')
-    # run_efficientnet_baseline_regression(experiment='./experiments/regression/baseline/config_aptos_4.yml', gpu='6,7')
-    # run_efficientnet_baseline_regression(experiment='./experiments/regression/baseline/config_aptos_8.yml', gpu='6,7')
-    # run_efficientnet_baseline_regression(experiment='./experiments/regression/baseline/config_aptos_UB.yml', gpu='6,7')
-    # #
-    # #adv models
-    # for i in range(1,11):
-    #     run_efficientnet_adv_early_regression(experiment=f'./experiments/regression/adversarial_network_train_val_early/config_aptos_2_{i}.yml', gpu='5,6,7')
-    # for i in range(1,11):
-    #     run_efficientnet_adv_early_regression(experiment=f'./experiments/regression/adversarial_network_train_val_early/config_aptos_4_{i}.yml', gpu='5,6,7')
-    # for i in range(1, 11):
-    #     run_efficientnet_adv_early_regression(experiment=f'./experiments/regression/adversarial_network_train_val_early/config_aptos_8_{i}.yml', gpu='5,6,7')
-    #
-    # # adv models
-    # for i in range(1, 11):
-    #     run_efficientnet_adv_late_regression(
-    #         experiment=f'./experiments/regression/adversarial_network_train_val_late/config_aptos_2_{i}.yml',
-    #         gpu='5,6,7')
-    # for i in range(1, 11):
-    #     run_efficientnet_adv_late_regression(
-    #         experiment=f'./experiments/regression/adversarial_network_train_val_late/config_aptos_4_{i}.yml',
-    #         gpu='5,6,7')
-    # for i in range(1, 11):
-    #     run_efficientnet_adv_late_regression(
-    #         experiment=f'./experiments/regression/adversarial_network_train_val_late/config_aptos_8_{i}.yml',
-    #         gpu='5,6,7')
-    #
-    #pre-trained models
-    # run_pre_trained_regression(
-    #     experiment='./experiments/regression/pre_trained/config_aptos_2_contrastive.yml', gpu='7')
+    # baseline, without pre-train
+    run_baseline_regression(experiment='./experiments/regression/baseline/config_aptos_2.yml')
+    run_baseline_regression(experiment='./experiments/regression/baseline/config_aptos_4.yml')
+    run_baseline_regression(experiment='./experiments/regression/baseline/config_aptos_8.yml')
+    run_baseline_regression(experiment='./experiments/regression/baseline/config_aptos_UB.yml')
 
-    # run_efficientnet_adv_early_regression(
-    #     experiment=experiment, gpu='6,7')
+    # pre-trained contrastive (SimCLR)
+    run_pre_trained_regression(
+        experiment='./experiments/regression/pre_trained/config_aptos_2_contrastive.yml'
+    )
+    run_pre_trained_regression(
+        experiment='./experiments/regression/pre_trained/config_aptos_4_contrastive.yml'
+    )
+    run_pre_trained_regression(
+        experiment='./experiments/regression/pre_trained/config_aptos_8_contrastive.yml'
+    )
 
-    ###########################################################################
+    # pre-trained rotation
+    run_pre_trained_regression(experiment='./experiments/regression/pre_trained/config_aptos_2_rotation.yml')
+    run_pre_trained_regression(experiment='./experiments/regression/pre_trained/config_aptos_4_rotation.yml')
+    run_pre_trained_regression(experiment='./experiments/regression/pre_trained/config_aptos_8_rotation.yml')
 
-    #HIST
-    #run_efficientnet_baseline_classification(experiment='./experiments/classification/baseline/config_HIST_8.yml', gpu='7')
+    # pre-trained patch (CP)
+    run_pre_trained_regression(experiment='./experiments/regression/pre_trained/config_aptos_2_patch.yml')
+    run_pre_trained_regression(experiment='./experiments/regression/pre_trained/config_aptos_4_patch.yml')
+    run_pre_trained_regression(experiment='./experiments/regression/pre_trained/config_aptos_8_patch.yml')
 
-    # run_efficientnet_adv_early_classification(
-    #     experiment=experiment, gpu='4,5')
+    # variance-aware, ealy agg
+    run_adv_early_regression(
+        experiment='./experiments/regression/adversarial_network_train_val_early/config_aptos_2.yml'
+    )
+    run_adv_early_regression(
+        experiment='./experiments/regression/adversarial_network_train_val_early/config_aptos_4.yml'
+    )
+    run_adv_early_regression(
+        experiment='./experiments/regression/adversarial_network_train_val_early/config_aptos_8.yml'
+    )
+
+    # variance-aware, late agg
+    run_adv_late_regression(
+        experiment='./experiments/regression/adversarial_network_train_val_late/config_aptos_2.yml'
+    )
+    run_adv_late_regression(
+        experiment='./experiments/regression/adversarial_network_train_val_late/config_aptos_4.yml'
+    )
+    run_adv_late_regression(
+        experiment='./experiments/regression/adversarial_network_train_val_late/config_aptos_8.yml'
+    )
 
     return None
 
